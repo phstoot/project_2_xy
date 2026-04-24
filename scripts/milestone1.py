2abbc56de01c12aae6b5cf5b24a3a6ef79aaf00e
phstoot <stoot.philip@gmail.com>
2026-04-24 12:37:05 +0200

    static plot

--- scripts/milestone1.py
+++ scripts/milestone1.py
@@ -14,22 +14,46 @@ class MonteCarlo_XY:
         self.temperature = temperature
         self.spins = self._init_spins()
         self._status = 'initialized'
-        pass
 
     def __repr__(self) -> str:
         return (
             f"MonteCarlo_XY(length={self.length_xy}, temperature={self.temperature:.2f}, "
             f"num_particles={self.length_xy**2}, step= TO BE IMPLEMENTED, "
+            f"status={self._status}"
             )
-        pass
 
     def _init_spins(self):
         spins = np.random.uniform(-np.pi, np.pi, (self.length_xy, self.length_xy))
         return spins
 
     def static_plot(self):
-        """Do a static visualisation of current system state
+        """Do a static visualisation of current system state using plt.quiver
         """
+        # create meshgrid for quiver
+        X, Y = np.meshgrid(np.arange(self.length_xy), np.arange(self.length_xy)) #optional it says
+        # angles = np.rad2deg(self.spins)
+
+        U = np.cos(self.spins)
+        V = np.sin(self.spins)
+        fig, ax = plt.subplots(figsize=(10,10))
+        
+        q = ax.quiver(
+            X, Y, U, V,
+            self.spins,
+            cmap='hsv', 
+            clim=[-np.pi, np.pi],
+            pivot='mid',
+            # scale_units='xy',      # Scale arrows relative to x,y axes
+            # scale=4,             # Inverse scale factor (higher = shorter arrows)
+            # width=0.003,           # Arrow shaft width
+            # headwidth=3,           # Head width as multiple of shaft width
+            # headlength=4,          # Head length as multiple of shaft width
+            # headaxislength=3.5,    # Head length at shaft intersection
+        )
+        plt.colorbar(q, label='Angle (radians)')
+        ax.set_aspect('equal')
+        plt.tight_layout()
+        plt.show()
 
     def step(self): 
         pass
