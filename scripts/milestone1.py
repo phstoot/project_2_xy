d12f13dd45ebfbefef854e95ebad67071fb00b91
phstoot <stoot.philip@gmail.com>
2026-04-24 11:59:30 +0200

    first lines

---
+++ scripts/milestone1.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tqdm
+
+class MonteCarlo_XY:
+    """_summary_
+    """
+    def __init__(
+            self,
+            length_xy : int = 4,
+            temperature : float = 1,
+            ):
+        self.length_xy = length_xy
+        self.temperature = temperature
+        self.spins = self._init_spins()
+        self._status = 'initialized'
+        pass
+
+    def __repr__(self) -> str:
+        return (
+            f"MonteCarlo_XY(length={self.length_xy}, temperature={self.temperature:.2f}, "
+            f"num_particles={self.length_xy**2}, step= TO BE IMPLEMENTED, "
+            )
+        pass
+
+    def _init_spins(self):
+        spins = np.random.uniform(-np.pi, np.pi, (self.length_xy, self.length_xy))
+        return spins
+
+    def static_plot(self):
+        """Do a static visualisation of current system state
+        """
+
+    def step(self):
+        pass
+
