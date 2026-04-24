da92287aa2a8595d80af97d8f6768059c4ad4e9a
phstoot <stoot.philip@gmail.com>
2026-04-24 12:52:58 +0200

    fixed imports

--- scripts/testscript.py
+++ scripts/testscript.py
@@ -1,10 +1,17 @@
-import simul as mod
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from pathlib import Path
+
+# Add src directory to path
+src_dir = Path(__file__).resolve().parent.parent / "src"
+sys.path.insert(0, str(src_dir))
+
+import simul # src is added to path so direct import
 
 
 if __name__ == '__main__':
-    test = mod.MonteCarlo_XY(100, 1)
+    test = simul.MonteCarlo_XY(100, 1)
     print(test.spins)
     print(np.rad2deg(test.spins))
     test.static_plot()
