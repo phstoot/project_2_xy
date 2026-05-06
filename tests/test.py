365eed3866245c321cbcb25a7bd7adeae0593103
phstoot <stoot.philip@gmail.com>
2026-05-06 18:25:48 +0200

    small change

--- tests/test.py
+++ tests/test.py
@@ -12,19 +12,19 @@ if __name__ == '__main__':
     path = Path(__file__).resolve().parents[1]
     print(utils.find_project_root())
     
-    # temps = np.arange(0.5, 2.5, 0.2)
-    # sweeps = 2000 # 5 million markov steps
-    # print(f'\nStarting sim..')
-    # sim = simul.MonteCarlo_XY(50, 1, start='hot')
-    # sim.run(sweeps=sweeps, store=True, interval=20)
+    temps = np.arange(0.5, 2.5, 0.2)
+    sweeps = 2000 # 5 million markov steps
+    print(f'\nStarting sim..')
+    sim = simul.MonteCarlo_XY(50, 1, start='hot')
+    sim.run(sweeps=sweeps, store=True, interval=20)
     # np.save('results/testmag.npy', np.array(sim.magn_hist))
-    # print('done')
-    # sim.static_image()
-    # plt.plot(sim.magn_hist)
-    # plt.xlabel('t (steps)')
-    # plt.ylabel('m (M/L^2)')
-    # # plt.xlim(0,steps)
-    # plt.title('Magnetization of XY model')
-    # plt.savefig(f'results/magn_{T}.pdf')
-    # plt.close('all')
+    print('done')
+    sim.static_image()
+    plt.plot(sim.magn_hist)
+    plt.xlabel('t (steps)')
+    plt.ylabel('m (M/L^2)')
+    # plt.xlim(0,steps)
+    plt.title('Magnetization of XY model')
+    # plt.savefig(f'results/magn.pdf')
+    plt.close('all')
 
